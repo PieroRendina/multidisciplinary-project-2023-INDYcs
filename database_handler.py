844d2bea6b5c04d48c31c2da7f98032f8b9b6e28
Piero Rendina <piero.rendina@mail.polimi.it>
2023-04-17 23:06:52 +0200

    start frontend edit

---
+++ database_handler.py
@@ -0,0 +1,33 @@
+import pymongo
+from urllib.parse import quote_plus
+
+
+def db_connection(username, password):
+    """
+    Method to instantiate a connection to a MongoDB Atlas instance
+    :param username: username to access the database
+    :param password: password to access the database
+    :return client: a client connected to the database instance
+    """
+    username = quote_plus(username)
+    password = quote_plus(password)
+    cluster = 'cluster0.hns6k.mongodb.net'
+    uri = 'mongodb+srv://' + username + ':' + password + '@' + cluster
+    client = pymongo.MongoClient(uri)
+    return client
+
+
+def get_frame_bounding_boxes(collection, movie_title, frame_id):
+    """
+    Method to retrieve the bounding boxes associated to a frame in a movie
+    :param collection: MongoDB collection which stores a document for each movie with its own array of frames
+    :param movie_title: title of the movie whose frame is to retrieve
+    :param frame_id: id of the frame to retrieve
+    :return bounding_boxes: bounding boxes associated to the requested frame_id
+    """
+    frame_info = collection.aggregate([{"$match": {"title": movie_title}},
+                                       {"$project": {"frame": {"$arrayElemAt": ["$frames", frame_id]}}}])
+    frame_info = list(frame_info)
+    # TODO: it's assuming that the frame exists and it will be the first to be returned
+    bounding_boxes = frame_info[0]["frame"]["Coordinates"]
+    return bounding_boxes
--- video_detection.py
+++ video_detection.py
@@ -3,11 +3,12 @@ import numpy as np
 import torch.cuda
 from torchvision import transforms
 from transformers import CLIPProcessor, CLIPModel
-import matplotlib.pyplot as plt
-import matplotlib.patches as pltpatches
 from tqdm.notebook import tqdm
-import threading
 import time
+
+movie_url_dict = {
+    "iron man vs loki": "E:\\Yolo_v3_pretrained\\dataset\\videos\\Iron Man vs Loki - We have a Hulk - Suit Up Scene  The Avengers (2012) Movie Clip HD.mp4"
+}
 
 
 def frame_to_tensor(frame: np.ndarray):
@@ -64,10 +65,10 @@ def run_inference(model, processor, device, prompt, patches, patch_size, window,
     scores = torch.zeros(patches.shape[1], patches.shape[2])
     runs = torch.ones(patches.shape[1], patches.shape[2])
 
-    for Y in range(0, patches.shape[1]-window+1, stride):
-        for X in range(0, patches.shape[2]-window+1, stride):
+    for Y in range(0, patches.shape[1] - window + 1, stride):
+        for X in range(0, patches.shape[2] - window + 1, stride):
             big_patch = torch.zeros(patch_size * window, patch_size * window, 3)
-            patch_batch = patches[0, Y:Y+window, X:X+window]
+            patch_batch = patches[0, Y:Y + window, X:X + window]
             for y in range(window):
                 for x in range(window):
                     big_patch[
@@ -79,20 +80,20 @@ def run_inference(model, processor, device, prompt, patches, patch_size, window,
                 return_tensors="pt",  # tell CLIP to return pytorch tensor
                 text=prompt,  # class label sent to CLIP
                 padding=True
-            ).to(device) # move to device if possible
+            ).to(device)  # move to device if possible
 
             # calculate and retrieve similarity score
             score = model(**inputs).logits_per_image.item()
             # sum up similarity scores from current and previous big patches
             # that were calculated for patches within the current window
-            scores[Y:Y+window, X:X+window] += score
+            scores[Y:Y + window, X:X + window] += score
             # calculate the number of runs on each patch within the current window
-            runs[Y:Y+window, X:X+window] += 1
+            runs[Y:Y + window, X:X + window] += 1
     # calculate average scores
     scores /= runs
     # clip scores
     for _ in range(3):
-        scores = np.clip(scores-scores.mean(), 0, np.inf)
+        scores = np.clip(scores - scores.mean(), 0, np.inf)
     # normalize scores
     scores = (scores - scores.min()) / (scores.max() - scores.min())
     return scores
@@ -101,8 +102,8 @@ def run_inference(model, processor, device, prompt, patches, patch_size, window,
 def get_box(scores, patch_size, threshold):
     detection = scores > threshold
     # find box corners
-    y_min, y_max = np.nonzero(detection)[:, 0].min().item(), np.nonzero(detection)[:, 0].max().item()+1
-    x_min, x_max = np.nonzero(detection)[:, 1].min().item(), np.nonzero(detection)[:, 1].max().item()+1
+    y_min, y_max = np.nonzero(detection)[:, 0].min().item(), np.nonzero(detection)[:, 0].max().item() + 1
+    x_min, x_max = np.nonzero(detection)[:, 1].min().item(), np.nonzero(detection)[:, 1].max().item() + 1
     # convert from patch co-ords to pixel co-ords
     y_min *= patch_size
     y_max *= patch_size
@@ -146,28 +147,20 @@ def detect(model, processor, device, prompts, frame, patch_size=64, window=3, st
         x, y, width, height = get_box(scores, patch_size, threshold)
         # create the bounding box
         # rect = pltpatches.Rectangle((x, y), width, height, linewidth=3, edgecolor=colors[i], facecolor='none')
-        cv2.rectangle(frame, (x, y), (x+width, y+height), [0, 255, 0])
+        cv2.rectangle(frame, (x, y), (x + width, y + height), [0, 255, 0])
         # add the patch to the Axes
         # ax.add_patch(rect)
     cv2.imshow("Frame", frame)
 
 
-
-def show_video_and_detect(input_file_path, prompts):
-    """
-    Function to show the video in an external window.
-    When the video is paused the detection algorithm is run with the specified prompts.
-    @param: input_file_path path of the video to be shown
-    """
-    # Show the video
-    capture = cv2.VideoCapture(input_file_path)
-    frame_width = capture.get(cv2.CAP_PROP_FRAME_WIDTH)
-    frame_height = capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+def show_video_and_detect(movie_title: str, frame_per_second: float):
+    url = movie_url_dict[movie_title.lower()]
+    capture = cv2.VideoCapture(url)
+    current_frame = 0
+    t0 = time.time()
     fps = np.ceil(capture.get(cv2.CAP_PROP_FPS))
-    print(f"fps:{fps:.2f}, frame width: {frame_width}, frame height: {frame_height}")
-
-    model, processor, device = load_model()
-
+    skip_frames = int(fps / frame_per_second)
+    # Till the end of the video
     while capture.isOpened():
         ret, frame = capture.read()
 
@@ -179,9 +172,9 @@ def show_video_and_detect(input_file_path, prompts):
                 break
             elif key == 32:
                 t0 = time.time()
-                detect(model, processor, prompts=prompts, device=device, frame=frame)
+                # TODO Add database query
                 t1 = time.time()
-                print(f"Time for detection = {t1-t0}")
+                print(f"Time for retrieval = {t1 - t0}")
                 cv2.waitKey()
         # Break the loop
         else:
@@ -195,5 +188,5 @@ def show_video_and_detect(input_file_path, prompts):
 
 
 if __name__ == '__main__':
-    show_video_and_detect("E:\\Yolo_v3_pretrained\\dataset\\videos\\Iron Man vs Loki - We have a Hulk - Suit Up Scene  The Avengers (2012) Movie Clip HD.mp4",
-                          prompts=["black t-shirt"])
+    movie_title = "iron man vs loki"
+    print(movie_url_dict[movie_title.lower()])
